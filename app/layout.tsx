765595a8a1452c8fcc79ef0d6fa5f9b383189823
badrinarayanms <badrinarayan.moola@gmail.com>
2024-09-08 13:11:26 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
